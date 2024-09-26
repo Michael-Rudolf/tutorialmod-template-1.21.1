ea3a60a55da24ce0da5594a4e522b07757060f7b
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2024-09-26 17:50:39 +0200

    Started adding blocks of ruby

--- src/main/java/net/michaelrudolf/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/michaelrudolf/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.michaelrudolf.tutorialmod.block;
 
 import net.michaelrudolf.tutorialmod.TutorialMod;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -9,7 +10,14 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-
+    public static final Block RUBY_BLOCK = registerBlock(
+            "ruby_block",
+            new Block(
+                    AbstractBlock.Settings.create()
+                            .resistance(10.0F)
+                            .hardness(5.0F)
+                             .requiresTool()
+             ));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
--- src/main/java/net/michaelrudolf/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/michaelrudolf/tutorialmod/item/ModItemGroups.java
@@ -2,6 +2,7 @@ package net.michaelrudolf.tutorialmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.michaelrudolf.tutorialmod.TutorialMod;
+import net.michaelrudolf.tutorialmod.block.ModBlocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -15,10 +16,12 @@ public class ModItemGroups {
             Identifier.of(TutorialMod.MOD_ID, "ruby"),
             FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
                     .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        // Items
                         entries.add(ModItems.RUBY);
                         entries.add(ModItems.RAW_RUBY);
 
-
+                        // Blocks
+                        entries.add(ModBlocks.RUBY_BLOCK);
                     }).build());
 
     public static void registerItemGroups() {
